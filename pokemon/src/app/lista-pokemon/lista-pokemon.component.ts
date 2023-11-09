ac4c0b8ad095f7b00397060ea9d028b69a02e46c
elshahat-khaled <elshahat.khaled@iisgalvanimi.edu.it>
2023-11-09 12:55:22 +0000

    method param

--- pokemon/src/app/lista-pokemon/lista-pokemon.component.ts
+++ pokemon/src/app/lista-pokemon/lista-pokemon.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
+import { Location } from '@angular/common'
 @Component({
   selector: 'app-lista-pokemon',
   templateUrl: './lista-pokemon.component.html',
   styleUrls: ['./lista-pokemon.component.css']
 })
 export class ListaPokemonComponent {
+  routepokemon: Observable<ParamMap> | undefined;
+    constructor(
+      private route: ActivatedRoute,
+    ){
+      
+     }
+  
+    ngOnInit(): void {
+  
+      this.routepokemon = this.route.paramMap;
+      this.routepokemon.subscribe(this.getRouterParam);
+    }
+  
+    getRouterParam = (params: ParamMap) => {
+      console.log(params);
+      let itemId = params.get('id');
+      console.log(itemId);
+      
+      
+    };
+  
+  
   
 
 }
